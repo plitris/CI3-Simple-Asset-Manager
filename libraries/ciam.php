ec44fe7d069fbf9ce94cc272c3479371f10002e1
Dmitriy Kravchuk <dmitriy.kravchuk@ymail.com>
2016-07-07 10:17:57 +0200

    Added more flexibility and efficiency.
    CSS and JS have separate main folder, so instead of css/style.css you can just type style.css
    
    Added resource support for favicons. href is automatically converted to direct link. Remove from library manually if not required.
    Added support for meta tags. Meta tags can be listed just like resources, and loaded when required. `msapplication-TileImage` meta tag content will automatically have direct link.

--- libraries/ciam.php
+++ libraries/ciam.php
@@ -14,7 +14,9 @@ class ciam{
     private $amc_js;
     
     //Main assets folder
-    private $assets_folder;
+    private $css_folder;
+    private $js_folder;
+    private $favicon_folder;
     
     
     
@@ -33,7 +35,9 @@ class ciam{
         $this->ci->config->load('ciam');
         
         //Set main assets folder
-        $this->assets_folder = $this->ci->config->item('assets_folder') . '/'; //Extra slash to compensate for additions to the URL
+        $this->css_folder = $this->ci->config->item('css_folder') . '/'; //Extra slash to compensate for additions to the URL
+        $this->js_folder = $this->ci->config->item('js_folder') . '/'; //Extra slash to compensate for additions to the URL
+        $this->favicon_folder = $this->ci->config->item('favicon_folder') . '/'; //Extra slash to compensate for additions to the URL
         
         //Get CSS items
         $this->amc_css = $this->ci->config->item('css');
@@ -45,14 +49,16 @@ class ciam{
     }
     
 
-    
+    //Outputs CSS based on they key name that has the links
+    //Base URL is automatically added
     function css($assets_keys){
         
         return $this->generate_html('css', $assets_keys);
         
     }
     
-    
+    //Outputs JS based on they key name that has the links
+    //Base URL is automatically added
     //[js][homepage] = array('source1.js', 'source2.js')
     function js($assets_keys){      //e.g. array('homepage', 'article')
         
@@ -60,35 +66,105 @@ class ciam{
         
     }
     
+    //Outputs Favicons with all the properties
+    //Base URL is automatically added to href
+    function fav($fav){
+        
+        return $this->generate_html('favicon', $fav);
+        
+    }
+    
+    //Outputs Favicons with all the properties
+    //Base URL is automatically added only to msapplication-TileImage 
+    //Uses $this->favicon_folder as the base
+    function meta($meta){
+        return $this->generate_html('meta', $meta);
+    }
+    
+    
+    
     private function generate_html($link_type, $assets_keys){ //Generate HTML for js or css link type
         $links = '';     //HTML for the links
         
         if(is_array($assets_keys)):
             //For each widget
             foreach ($assets_keys as $asset_list):
-                $assets = ($link_type == 'css')? $this->amc_css[$asset_list]:$this->amc_js[$asset_list];
+                $assets = $this->ci->config->item($link_type);
                 foreach ($assets as $link):
                     if($link_type == 'js'):
-                        $links .= '<script type="text/javascript" src="' . base_url($this->assets_folder . $link) . '"></script>';      //Building link HTML
+                        $links .= '<script type="text/javascript" src="' . base_url($this->js_folder . $link) . '"></script>';      //Building link HTML
                     elseif($link_type == 'css'):
-                        $links .= link_tag($this->assets_folder . $link);
+                        $links .= link_tag($this->css_folder . $link);
+                    elseif ($link_type == 'favicon'):
+                        $links .= $this->generate_favicon($link);
+                    elseif ($link_type == 'meta'):
+                        $links .= $this->generate_meta($link);
                     endif;
                 endforeach;
             endforeach;
         else:
-            $single_asset = ($link_type == 'css')? $this->amc_css : $this->amc_js;
+            $single_asset = $this->ci->config->item($link_type);
             if(array_key_exists($assets_keys, $single_asset)):
                 foreach ($single_asset[$assets_keys] as $s_link):
                     if($link_type == 'js'):
-                        $links .= '<script type="text/javascript" src="' . base_url($this->assets_folder . $s_link) . '"></script>' . PHP_EOL;
+                        $links .= '<script type="text/javascript" src="' . base_url($this->js_folder . $s_link) . '"></script>' . PHP_EOL;
                     elseif($link_type == 'css'):
-                        $links .= link_tag($this->assets_folder . $s_link);
+                        $links .= link_tag($this->css_folder . $s_link);
+                    elseif ($link_type == 'favicon'):
+                        $links .= $this->generate_favicon($s_link);
+                    elseif ($link_type == 'meta'):
+                        $links .= $this->generate_meta($s_link);
                     endif;
                 endforeach;
             endif;
         endif;
         
         return $links;
+    }
+    
+    function generate_favicon($favicon_props){
+        
+        $favicon_html = '<link';
+            
+        if (count($favicon_props) > 1):
+            foreach ($favicon_props as $key => $value):
+                if($key != 'href'):
+                    $favicon_html .= ' ' . $key . '="' . $value . '"';
+                endif;
+            endforeach;
+        endif;
+        
+        $favicon_html .= (array_key_exists('href', $favicon_props))?' href="' . base_url($this->favicon_folder . $favicon_props['href']) . '"':'';
+        
+        $favicon_html .= ' />' . PHP_EOL;
+        
+        return $favicon_html;
+        
+    }
+    
+    function generate_meta($meta_props){
+        
+        $meta_link = '<meta ';
+        
+        $meta_name_exists = array_key_exists('name', $meta_props);
+        $meta_link .= (array_key_exists('name', $meta_props))?' name="' . $meta_props['name'] . '"':'';
+        
+        if($meta_name_exists && $meta_props['name'] == 'msapplication-TileImage'):
+            $meta_link .= (array_key_exists('content', $meta_props))?' content="' . base_url($this->favicon_folder . $meta_props['content']) . '"':'';
+        else:
+            $meta_link .= (array_key_exists('content', $meta_props))?' content="' . $meta_props['content'] . '"':'';
+        endif;
+        
+        if (count($meta_props) > 2):
+            foreach ($meta_props as $key => $value):
+                if($key != 'name' || $key != 'content'):
+                    $meta_link .= ' ' . $key . '="' . $value . '"';
+                endif;
+            endforeach;
+        endif;
+        
+        return $meta_link . ' />' . PHP_EOL;
+        
     }
     
     
